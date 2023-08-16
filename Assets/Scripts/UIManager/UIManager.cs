4c92bdeaedc853cbc03e3409a6755f56a28ebcc6
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-08-16 15:31:53 +0900

    UI bar Animation clear
    UI bar 애니메이션 제작완료

--- Assets/Scripts/UIManager/UIManager.cs
+++ Assets/Scripts/UIManager/UIManager.cs
@@ -6,21 +6,31 @@ using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
 {
-
     public Transform build_panel;
     public Transform road_panel;
     public Transform shop_panel;
     public Transform production_panel;
+    public Transform build_button;
+    public Transform road_button;
+    public Transform shop_button;
+    public Transform production_button;
 
-    private bool isOn;
+    private bool buildisOn;
+    private bool roadisOn;
+    private bool shopisOn;
+    private bool prdouctionisOn;
 
     // Start is called before the first frame update
     void Awake()
     {
-        build_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-        road_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-        shop_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-        production_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
+        build_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+        road_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+        shop_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+        production_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+        build_button.LeanMoveLocalY(-515, 0.5f).setEaseInExpo();
+        road_button.LeanMoveLocalY(-515, 0.5f).setEaseInExpo();
+        shop_button.LeanMoveLocalY(-515, 0.5f).setEaseInExpo();
+        production_button.LeanMoveLocalY(-515, 0.5f).setEaseInExpo();
     }
 
     // Update is called once per frame
@@ -29,74 +39,114 @@ public class UIManager : MonoBehaviour
         
     }
 
+    public void movebutton_on()
+    {
+        build_button.LeanMoveLocalY(-285, 0.5f).setEaseOutExpo();
+        road_button.LeanMoveLocalY(-285, 0.5f).setEaseOutExpo();
+        shop_button.LeanMoveLocalY(-285, 0.5f).setEaseOutExpo();
+        production_button.LeanMoveLocalY(-285, 0.5f).setEaseOutExpo();
+    }
+
+    public void movebutton_off()
+    {
+        build_button.LeanMoveLocalY(-515, 0.5f).setEaseInExpo();
+        road_button.LeanMoveLocalY(-515, 0.5f).setEaseInExpo();
+        shop_button.LeanMoveLocalY(-515, 0.5f).setEaseInExpo();
+        production_button.LeanMoveLocalY(-515, 0.5f).setEaseInExpo();
+    }
+
     public void panelBarOnOff()
     {
         GameObject clickObject = EventSystem.current.currentSelectedGameObject;
 
         if(clickObject.name == "BuildingButton")
         {
-            if(!isOn)
+            if(!buildisOn)
             {
-                build_panel.LeanMoveLocalY(-360, 0.5f).setEaseOutExpo();
-                road_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                shop_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                production_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                isOn = true;
+                build_panel.LeanMoveLocalY(-420, 0.5f).setEaseOutExpo();
+                movebutton_on();
+                road_panel.LeanMoveLocalY(-650, 0.5f).setEaseInExpo();
+                shop_panel.LeanMoveLocalY(-650, 0.5f).setEaseInExpo();
+                production_panel.LeanMoveLocalY(-650, 0.5f).setEaseInExpo();
+
+                buildisOn = true;
+                roadisOn = false;
+                shopisOn = false;
+                prdouctionisOn = false;
             }
             else
             {
-                build_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                isOn = false;
+                build_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+                movebutton_off();
+                buildisOn = false;
             }
             
             
         }
         else if(clickObject.name == "RoadButton")
         {
-            if (!isOn)
+            if (!roadisOn)
             {
-                road_panel.LeanMoveLocalY(-360, 0.5f).setEaseOutExpo();
-                build_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                shop_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                production_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                isOn = true;
+                road_panel.LeanMoveLocalY(-420, 0.5f).setEaseOutExpo();
+                movebutton_on();
+                build_panel.LeanMoveLocalY(-650, 0.5f).setEaseInExpo();
+                shop_panel.LeanMoveLocalY(-650, 0.5f).setEaseInExpo();
+                production_panel.LeanMoveLocalY(-650, 0.5f).setEaseInExpo();
+                
+                buildisOn = false;
+                roadisOn = true;
+                shopisOn = false;
+                prdouctionisOn = false;
             }
             else
             {
-                road_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                isOn = false;
+                road_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+                movebutton_off();
+                roadisOn = false;
             }
         }
         else if(clickObject.name == "ShopButton")
         {
-            if (!isOn)
+            if (!shopisOn)
             {
-                shop_panel.LeanMoveLocalY(-360, 0.5f).setEaseOutExpo();
-                build_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                road_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                production_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                isOn = true;
+                shop_panel.LeanMoveLocalY(-420, 0.5f).setEaseOutExpo();
+                movebutton_on();
+                build_panel.LeanMoveLocalY(-650, 0.5f).setEaseInExpo();
+                road_panel.LeanMoveLocalY(-650, 0.5f).setEaseInExpo();
+                production_panel.LeanMoveLocalY(-650, 0.5f).setEaseInExpo();
+
+                buildisOn = false;
+                roadisOn = false;
+                shopisOn = true;
+                prdouctionisOn = false;
             }
             else
             {
-                shop_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                isOn = false;
+                shop_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+                movebutton_off();
+                shopisOn = false;
             }
         }
         else if(clickObject.name == "ProductionButton")
         {
-            if (!isOn)
+            if (!prdouctionisOn)
             {
-                production_panel.LeanMoveLocalY(-360, 0.5f).setEaseOutExpo();
-                build_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                road_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                shop_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                isOn = true;
+                production_panel.LeanMoveLocalY(-420, 0.5f).setEaseOutExpo();
+                movebutton_on();
+                build_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+                road_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+                shop_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+
+                buildisOn = false;
+                roadisOn = false;
+                shopisOn = false;
+                prdouctionisOn = true;
             }
             else
             {
-                production_panel.LeanMoveLocalY(-590, 0.5f).setEaseInExpo();
-                isOn = false;
+                production_panel.LeanMoveLocalY(-655, 0.5f).setEaseInExpo();
+                movebutton_off();
+                prdouctionisOn = false;
             }
         }
     }
